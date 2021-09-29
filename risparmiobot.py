32ec82fe3379e4cc6bcd186013897e417b4fe75e
renato-milano <renatomilano96@libero.it>
2021-09-29 21:35:36 +0200

    Increase time to load no number more

--- risparmiobot.py
+++ risparmiobot.py
@@ -161,7 +161,8 @@ def searchProductIMG(update,context):
         userAgent = ua.random
         chrome_options.add_argument(f'user-agent={userAgent}')
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         #chrome_options.add_argument("window-size=1400,800")
@@ -196,12 +197,13 @@ def searchProductText(update,context):
             #opts = Options()
             #opts.headless=True
             chrome_options = webdriver.ChromeOptions()
-            chrome_options = webdriver.ChromeOptions()
             ua = UserAgent()
             userAgent = ua.random
             chrome_options.add_argument(f'user-agent={userAgent}')
             chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-            #chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument('--disable-gpu')
+            
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--no-sandbox")
             #chrome_options.add_argument("window-size=1400,800")
